a3324021cc4ad1f61f7b8a859c26f71805df19cf
Lancai <araraaraiguma@gmail.com>
2024-06-03 23:34:16 +0900

    Replace existing app with the new app

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material"
+import PersistentDrawerLeft from "@/components/PersistentDrawer";
+
+export default function Home() {
+  return (
+    <div>
+      <PersistentDrawerLeft />
+    </div>
+  );
+}
